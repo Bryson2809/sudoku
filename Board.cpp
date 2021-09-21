9e4e902b133fc06c308572fea2bd27fde1e4551c
bryson2809 <jbc2@email.sc.edu>
2021-09-21 12:03:48 -0400

    set up for random board selection

--- Board.cpp
+++ Board.cpp
@@ -2,13 +2,18 @@
 #include <cstdlib>
 #include <string>
 #include <time.h>
+#include <array>
+
 
 using namespace std;
 
 class Board
 {
 public:
-	int nums[9][9] = { 
+
+	array<array<int, 9>, 9> finalBoard; 
+	
+	array<array<int, 9>, 9> board0 = { {
 		{0,0,4,0,5,0,0,0,0},
 		{9,0,0,7,3,4,6,0,0},
 		{0,0,3,0,2,1,0,4,9},
@@ -18,7 +23,25 @@ public:
 		{3,1,0,9,7,0,2,0,0},
 		{0,0,9,1,8,2,0,0,3},
 		{0,0,0,0,6,0,1,0,0}
-	};
+	} };
+
+	array<array<int, 9>, 9> board1;
+
+	array<array<int, 9>, 9> board2;
+
+	array<array<int, 9>, 9> board3;
+
+	array<array<int, 9>, 9> board4;
+
+	array<array<int, 9>, 9> board5;
+
+	array<array<int, 9>, 9> board6;
+
+	array<array<int, 9>, 9> board7;
+
+	array<array<int, 9>, 9> board8;
+
+	array<array<int, 9>, 9> board9;
 	Board()
 	{
 		
@@ -31,35 +54,36 @@ public:
 
 	void toString()
 	{
-		for (int i = 0; i < sizeof(this->nums[i]) / sizeof(int); i++)
+		cout << endl;
+		for (int i = 0; i < sizeof(this->finalBoard[i]) / sizeof(int); i++)
 		{
 			cout << "  ";
-			for (int j = 0; j < sizeof(this->nums[j]) / sizeof(int); j++)
+			for (int j = 0; j < sizeof(this->finalBoard[j]) / sizeof(int); j++)
 			{
 				if ((j + 1) % 3 != 0)
 				{
-					if (this->nums[i][j] == 0)
+					if (this->finalBoard[i][j] == 0)
 						cout << "x ";
 					else
-						cout << this->nums[i][j] << " ";
+						cout << this->finalBoard[i][j] << " ";
 				}
 					
-				else //if ((j + 1) % 3 == 0)
+				else
 				{
 					if (j == 8)
 					{
-						if (this->nums[i][j] == 0)
+						if (this->finalBoard[i][j] == 0)
 							cout << "x";
 						else
-							cout << this->nums[i][j];
+							cout << this->finalBoard[i][j];
 					}
 					
 					else
 					{
-						if (this->nums[i][j] == 0)
+						if (this->finalBoard[i][j] == 0)
 							cout << "x | ";
 						else
-							cout << this->nums[i][j] << " | ";
+							cout << this->finalBoard[i][j] << " | ";
 					}
 				}
 			}
@@ -68,11 +92,51 @@ public:
 			else if (i != 8)
 				cout << "\n" << "-------------------------" << endl;
 		}
+
+		cout << endl;
 		cout << endl;
 	}
 
 	void randBoard()
 	{
-		//srand
+		srand(time(NULL));
+
+		int boardNum = rand() % 10;
+
+		finalBoard = board0;
+
+		/*switch (boardNum)
+		{
+		case 0:
+			finalBoard = board0;
+			break;
+		case 1:
+			finalBoard = board1;
+			break;
+		case 2:
+			finalBoard = board2;
+			break;
+		case 3:
+			finalBoard = boards3;
+			break;
+		case 4:
+			finalBoard = board4;
+			break;
+		case 5:
+			finalBoard = board5;
+			break;
+		case 6:
+			finalBoard = board6;
+			break;
+		case 7:
+			finalBoard = board7;
+			break;
+		case 8:
+			finalBoard = board8;
+			break;
+		case 9:
+			finalBoard = board9;
+			break;
+		}*/
 	}
 };
--- Driver.cpp
+++ Driver.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include "Board.cpp"
 #include <time.h>
+#include <array>
+#include "Board.cpp"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	Board board;
+	
+	board.randBoard();
 
 	board.toString();
 
